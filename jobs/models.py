46e02dba26afc0df948129c543576404677168e0
Shubham Bisht <shubhambisht0308@gmail.com>
2019-07-28 09:08:32 +0530

    Created admin and how to add images

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
