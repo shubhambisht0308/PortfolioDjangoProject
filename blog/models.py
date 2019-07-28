8c6e7abb8eff2927d8738e3c03d56bf0f3d68cf6
Shubham Bisht <shubhambisht0308@gmail.com>
2019-07-28 18:36:32 +0530

    Added the blog app to the project

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
